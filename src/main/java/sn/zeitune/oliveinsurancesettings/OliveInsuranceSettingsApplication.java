58d5d2c36cdca3a76611bbf0c8d93353409665d7
Mouhamadou Tidiane Seck <mtseckdev@gmail.com>
2025-06-30 13:19:12 +0000

    Refine .gitignore to ignore only application.yml

--- src/main/java/sn/zeitune/oliveinsurancesettings/OliveInsuranceSettingsApplication.java
+++ src/main/java/sn/zeitune/oliveinsurancesettings/OliveInsuranceSettingsApplication.java
@@ -2,9 +2,19 @@ package sn.zeitune.oliveinsurancesettings;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class OliveInsuranceSettingsApplication {
+
+    @GetMapping
+    public String home() {
+        return "Welcome to Olive Insurance Settings Service!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(OliveInsuranceSettingsApplication.class, args);
