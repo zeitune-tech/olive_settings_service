8393f4c8c245e4c289ea1b4f3081a313333d69c5
Mouhamadou Tidiane Seck <mtseckdev@gmail.com>
2025-07-07 10:26:51 +0000

    Remove AttestationIssued and CompanyAttestations models
    Deleted AttestationIssued, CompanyAttestations, and related components such as services, controllers, mappers, DTOs, and repositories. These entities and their functionality are no longer required, simplifying the overall system.

--- src/main/java/sn/zeitune/oliveinsurancesettings/security/SecurityConfig.java
+++ src/main/java/sn/zeitune/oliveinsurancesettings/security/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
                         .requestMatchers("/inter-services/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/app/**").hasRole("USER")
+                        .requestMatchers("/").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
